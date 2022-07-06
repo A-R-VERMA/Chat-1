dea489a7b5f8cd9e03513d889805a9b1b40d6f5f
A-R-VERMA <104203886+A-R-VERMA@users.noreply.github.com>
2022-07-06 12:20:20 +0800

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBYNNuEGPVk0Aad-OluCy4faHx-xTo7nR8",
+    authDomain: "let-s-chat-web-app-18311.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-18311-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-18311",
+    storageBucket: "let-s-chat-web-app-18311.appspot.com",
+    messagingSenderId: "498614590981",
+    appId: "1:498614590981:web:9be170294cff60c3e6066c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
